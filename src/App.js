ce3fbf6ae8eabb1547aa467eb36e2c8c256466c9
jiro <j.code.public@gmail.com>
2020-04-16 17:34:38 -0700

    add style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import { GlobalStyle } from './styles'
 
-const App = () => <h1>Hello World</h1>
+const App = () => (
+  <>
+    <GlobalStyle />
+    <h1>Hello World</h1>
+  </>
+)
 
 export default App
