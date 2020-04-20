3f109f2aa4b28bf0f11fb7919d0174119a30ea7f
version-1 <j.onodera26@gmail.com>
2020-04-19 22:17:46 -0700

    implement container

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
 import reducer from './reducers'
 import App from './App'
 
-const store = createStore(reducer)
+const store = createStore(reducer, devToolsEnhancer())
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers/issue.js
+++ src/reducers/issue.js
@@ -1,5 +1,30 @@
 const initialState = {
-  data: {}
+  data: {
+    1: {
+      id: 1,
+      title: 'Issue 1',
+      status: 0,
+      assignee: 'jjoo',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    2: {
+      id: 2,
+      title: 'Issue 2',
+      status: 1,
+      assignee: 'jjoo',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    3: {
+      id: 3,
+      title: 'Issue 3',
+      status: 0,
+      assignee: 'jjoo',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  }
 }
 
 const reducer = (state = initialState, action) => {
