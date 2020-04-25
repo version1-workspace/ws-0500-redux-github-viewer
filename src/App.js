4316ec6ead7d7c6fedae2fcd8f0a35ce55caabac
jiro <j.code.public@gmail.com>
2020-04-24 21:08:42 -0700

    some fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React from 'react'
+import styled from 'styled-components'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { GlobalStyle } from './styles'
-import Pages from './pages/Index'
-import { BrowserRouter as Router } from 'react-router-dom'
+import Profile from './pages/Profile'
+import Index from './pages/Index'
+import Header from './components/organisms/Header'
+import IssueNew from './pages/issue/New'
 
-const App = () => (
-  <>
+const Container = styled.div``
+const Content = styled.div`
+  max-width: 896px;
+  margin: 0 auto;
+  padding: 32px 16px;
+`
+
+const App = () => {
+  return (
     <Router>
-      <GlobalStyle />
-      <Pages />
+      <Container>
+        <GlobalStyle />
+        <Header />
+        <Content>
+          <Switch>
+            <Route path="/issue/new">
+              <IssueNew />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route exact path="/">
+              <Index />
+            </Route>
+          </Switch>
+        </Content>
+      </Container>
     </Router>
-  </>
-)
+  )
+}
 
 export default App
