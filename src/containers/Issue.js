110100f840edcf6dea357d95b02a3933f1773da5
version-1 <j.onodera26@gmail.com>
2020-04-25 21:16:33 -0700

    implement issue page and pull-request page

--- src/containers/Issue.js
+++ src/containers/Issue.js
@@ -1,6 +1,6 @@
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import IssueTab from '../components/templates/Issue'
+import Issue from '../components/templates/Issue'
 import { actionCreators } from '../actions'
 
 const mapStateToProps = ({ user, issue }) => {
@@ -14,4 +14,4 @@ const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(actionCreators, dispatch)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(IssueTab)
+export default connect(mapStateToProps, mapDispatchToProps)(Issue)
