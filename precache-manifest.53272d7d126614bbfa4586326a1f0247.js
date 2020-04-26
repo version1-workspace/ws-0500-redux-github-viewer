239d4f091564481d5fde3a2753ccfeccf63a8478
jiro <j.code.public@gmail.com>
2020-04-26 11:51:47 -0700

    add new file

---
+++ precache-manifest.53272d7d126614bbfa4586326a1f0247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568774eb703ae8d476cb3d991af9f2c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec5e2ad8666d779ce5bd",
+    "url": "/static/js/2.8494beeb.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.8494beeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f8ad3fd3b70589a5d8",
+    "url": "/static/js/main.05fb81bc.chunk.js"
+  },
+  {
+    "revision": "24fb3c46679878512674",
+    "url": "/static/js/runtime-main.1dcfde97.js"
+  }
+]);
